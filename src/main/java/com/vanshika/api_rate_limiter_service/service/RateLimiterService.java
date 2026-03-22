abf14d09d9b91cf6dcd4d06f0d5003a0a9e8a51d
Vanshika vijay <vanshikavijay0304@gmail.com>
2026-03-23 01:35:30 +0530

    Phase 2 - Completion

--- src/main/java/com/vanshika/api_rate_limiter_service/service/RateLimiterService.java
+++ src/main/java/com/vanshika/api_rate_limiter_service/service/RateLimiterService.java
@@ -9,7 +9,6 @@ import com.vanshika.api_rate_limiter_service.repository.InMemoryBucketRepository
 import com.vanshika.api_rate_limiter_service.repository.RedisBucketRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
