7f7fb482f9e17165f48dda46e4582c1f747ca09b
Vanshika vijay <vanshikavijay0304@gmail.com>
2026-06-03 18:30:04 +0530

    Main file fix

--- src/main/java/com/vanshika/api_rate_limiter_service/ApiRateLimiterServiceApplication.java
+++ src/main/java/com/vanshika/api_rate_limiter_service/ApiRateLimiterServiceApplication.java
@@ -9,8 +9,8 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 /**
  * Main entry point for the API Rate Limiter Service.
  * 
- * @EnableScheduling is required to trigger periodic cleanup of expired token 
- * buckets in the repository layer.
+ * @EnableScheduling is required to trigger periodic cleanup of expired token
+ *                   buckets in the repository layer.
  */
 @SpringBootApplication
 @EnableScheduling
